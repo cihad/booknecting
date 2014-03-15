b4993d1795e426f2ecee19afe1218b3903ec3b69
Cihad Paksoy <cihadp@gmail.com>
2014-03-15 15:24:29 +0200

    sign in & sign out links on header

--- spec/views/application/_header.html.erb_spec.rb
+++ spec/views/application/_header.html.erb_spec.rb
@@ -1,13 +1,26 @@
 require 'spec_helper'
 
-describe "rendering messages" do
-
-  before do
-    render 'application/header'
-  end
+describe "application/_header.html.erb" do
 
   it "display the alert and the notice" do
+    render
     expect(rendered).to have_selector 'a', text: 'Booknecting'
+  end
+
+  describe "when user signed in" do
+    it "displays sign out link" do
+      view.stub(:user_signed_in?).and_return(true)
+      render
+      expect(rendered).to have_selector 'a', text: I18n.t('users.signout')
+    end
+  end
+
+  describe "when user sign out" do
+    it "displays sign in link" do
+      view.stub(:user_signed_in?).and_return(false)
+      render
+      expect(rendered).to have_selector 'a', text: I18n.t('users.signin')
+    end
   end
 
 end
