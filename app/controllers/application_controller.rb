faf61e2517ba7d1a7d584c09d91d1a6cba624a83
Cihad Paksoy <cihadp@gmail.com>
2014-03-15 15:25:22 +0200

    devise parameter sanitizer for account_update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).concat [:username, :first_name, :last_name]
+    attrs = [:username, :first_name, :last_name]
+    devise_parameter_sanitizer.for(:sign_up).concat attrs
+    devise_parameter_sanitizer.for(:account_update).concat attrs
   end
 end
