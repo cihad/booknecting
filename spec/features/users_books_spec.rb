bd5a8540f77d6fb3ebfb96babffdd30bc9b665bb
Cihad Paksoy <cihadp@gmail.com>
2014-03-19 20:10:00 +0200

    live people who reat it count and avatar images

--- spec/features/users_books_spec.rb
+++ spec/features/users_books_spec.rb
@@ -26,4 +26,21 @@ describe "Users Books" do
     }.to change { user.books.count }.by(-1)
   end
 
+  it "when click read button increments people who reat it count", js: true do
+    login_as user, scope: :user
+
+    visit book_path book
+    click_on I18n.t('users.books.read')
+    expect(page).to have_selector '.ui.right.floated.label', 
+                  text: I18n.t('books.show.read_count', count: "1")
+  end
+
+  it "when click read button displays last read user on people who read it", js: true do
+    login_as user, scope: :user
+
+    visit book_path book
+    click_on I18n.t('users.books.read')
+    expect(page).to have_selector '.image', count: 1
+  end
+
 end
