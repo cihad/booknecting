52705c64bcd366b4e49da2fde57b972a5c9c5c6f
Cihad Paksoy <cihadp@gmail.com>
2014-03-23 14:32:57 +0200

    show read button if user signed in

--- spec/helpers/books_helper_spec.rb
+++ spec/helpers/books_helper_spec.rb
@@ -6,21 +6,31 @@ describe BooksHelper do
     let(:user) { FactoryGirl.create :user } 
     let(:book) { FactoryGirl.create :book } 
 
-    before do
-      allow(helper).to receive(:current_user).and_return(user)
+    describe "when user didnt sign in" do
+      it "returns nil" do
+        allow(helper).to receive(:user_signed_in?).and_return(false)
+        expect(helper.read_button(book)).to be_nil
+      end
     end
 
-    it "when user didnt read the book" do
-      expect(helper.read_button(book)).to have_selector 'button',
-                                          text: I18n.t('users.books.read')
-      expect(helper.read_button(book)).to_not have_selector '.green',
-                                          text: I18n.t('users.books.read')
-    end
+    describe "when user signed in" do
+      before do
+        allow(helper).to receive(:user_signed_in?).and_return(true)
+        allow(helper).to receive(:current_user).and_return(user)
+      end
 
-    it "when user read the book" do
-      user.books << book
-      expect(helper.read_button(book)).to have_selector '.green',
-                                          text: I18n.t('users.books.read')
+      it "when user didnt read the book" do
+        expect(helper.read_button(book)).to have_selector 'button',
+                                            text: I18n.t('users.books.read')
+        expect(helper.read_button(book)).to_not have_selector '.green',
+                                            text: I18n.t('users.books.read')
+      end
+
+      it "when user read the book" do
+        user.books << book
+        expect(helper.read_button(book)).to have_selector '.green',
+                                            text: I18n.t('users.books.read')
+      end
     end
   end
 
