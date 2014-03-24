a0b0e8d5338951015908e580c4c6cc2f6d1ad02f
Cihad Paksoy <cihadp@gmail.com>
2014-03-25 00:05:32 +0200

    book json with image and users

--- app/views/books/show.json.jbuilder
+++ app/views/books/show.json.jbuilder
@@ -1,1 +1,11 @@
-json.extract! @book, :id, :name
+json.id @book.id
+json.name @book.name
+
+json.image do
+  json.original root_url.chop + @book.image.url
+  json.medium root_url.chop + @book.image.medium.url
+end
+
+json.users do
+  json.array! @book.users, :id, :username, :view_name
+end
--- spec/requests/book_json_spec.rb
+++ spec/requests/book_json_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe "Book JSON" do
 
   let(:book) { FactoryGirl.create :book, name: "Example" }
+  let(:user) { FactoryGirl.create :user }
 
   subject { book }
 
   before do
+    book.users << user
     get book_path(subject, format: :json)
   end
 
@@ -16,6 +18,34 @@ describe "Book JSON" do
 
   it "has name" do
     expect(JSON.parse(response.body)["name"]).to eq("Example")
+  end
+
+  describe "image" do
+    it "has original" do
+      expect(JSON.parse(response.body)["image"]).to include("original")
+    end
+
+    it "has medium" do
+      expect(JSON.parse(response.body)["image"]).to include("medium")
+    end
+  end
+
+  describe "users" do
+    it "should be a Array" do
+      expect(JSON.parse(response.body)["users"]).to be_a(Array)  
+    end
+
+    it "has a user id" do
+      expect(JSON.parse(response.body)["users"].first).to include("id")
+    end
+
+    it "has a user username" do
+      expect(JSON.parse(response.body)["users"].first).to include("username")
+    end
+
+    it "has a user view name" do
+      expect(JSON.parse(response.body)["users"].first).to include("view_name")
+    end
   end
 
 end
