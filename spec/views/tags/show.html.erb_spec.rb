c109c8642e6237181f0b970a80a6e76349b16dcf
Cihad Paksoy <cihadp@gmail.com>
2014-04-30 13:08:21 +0300

    add tag books view

--- spec/views/tags/show.html.erb_spec.rb
+++ spec/views/tags/show.html.erb_spec.rb
@@ -2,15 +2,21 @@ require "spec_helper"
 
 describe "tags/show.html.erb" do
 
-  let(:tag) { stub_model Tag, name: "Sample Tag" }
+  let(:tag) { stub_model Tag, name: "risale-i nur" }
 
   before do
     assign(:tag, tag)
+    allow(tag).to receive(:books).and_return([stub_model(Book, name: "Lemalar")])
+    stub_template 'books/_book.html.erb' => '<%= book.name %>'
     render
   end
 
   it "displays tag name" do
-    expect(rendered).to match "Sample Tag" 
+    expect(rendered).to match "risale-i nur" 
+  end
+
+  it "displays books" do
+    expect(rendered).to match "Lemalar"
   end
 
 end
